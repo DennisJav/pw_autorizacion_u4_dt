54d0abe40e28100586cbe20ed4c4d782d50b1c75
DennisJav <denis.javier.tapia.ortiz@gmail.com>
2023-08-09 19:46:44 -0500

    Taller38

---
+++ src/main/java/com/ec/PwAutorizacionU4DtApplication.java
@@ -0,0 +1,13 @@
+package com.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwAutorizacionU4DtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwAutorizacionU4DtApplication.class, args);
+	}
+
+}
